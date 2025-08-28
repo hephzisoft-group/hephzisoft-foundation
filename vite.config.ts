559738e3b60b00704a45d833b6d7d26316a3f1f5
Hephzisoft <hephzisoft@gmail.com>
2025-08-28 11:03:07 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-})
+  plugins: [react(), cloudflare(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
